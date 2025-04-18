4b6fcdb50702efdb2719aad8c5cd5c19de0ce84a
aakashvanmali45 <aakashvanmali05@gmail.com>
2025-04-19 00:50:34 +0200

    codegraph creation test

--- review_agents/main.py
+++ review_agents/main.py
@@ -74,7 +74,7 @@ def read_diff():
 
             try:
                 code_graph = extract_graph_from_code(full_content)
-                store_codegraph(repo, code_graph)
+                store_codegraph(repo,filename, code_graph)
 
             except Exception as e:
                 print(f"Error parsing or storing graph for {filename}: {e}")
--- review_agents/mongo.py
+++ review_agents/mongo.py
@@ -1,12 +1,24 @@
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = MongoClient(os.getenv("MONGODB_URI"))
-db = client["codegraph_db"]
+db = client[os.getenv("DB_NAME")]
+collection = db[os.getenv("COLLECTION_NAME")]
 
-def store_codegraph(repo, codegraph):
-    db.graphs.replace_one({"repo": repo}, {"repo": repo, "graph": codegraph}, upsert=True)
 
-def get_codegraph(repo):
-    doc = db.graphs.find_one({"repo": repo})
-    return doc["graph"] if doc else None
+def store_codegraph(repo, file_path, codegraph):
+    """Store the code graph in MongoDB."""
+    collection.update_one(
+        {"repo": repo, "file_path": file_path},
+        {"$set": {"code_graph": codegraph}},
+        upsert=True
+    )
+
+    
+def get_codegraph(repo, file_path):
+    """Retrieve the code graph for a specific file in a repo."""
+    doc = collection.find_one({"repo": repo, "file_path": file_path})
+    return doc["code_graph"] if doc else None
